09b1a8e48e00e2b6262a2ba3e5e9be319a19ed44
karim <karimsouari101@gamil.com>
2022-09-10 18:46:41 +0000

    pushing to github

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
 const myNav = document.getElementById('mynav');
 const links = document.getElementsByClassName('nav-link');
-const icon = document.getElementById('icon')[0];
-console.log(links);
+const icon = document.getElementById('icon');
 window.onscroll = function () {
-  console.log(window.scrollY);
+  console.log(icon);
 
   if (window.scrollY >= 120) {
     myNav.classList.add('bg-white');
     myNav.classList.remove('bg-transparent');
+    icon.style.color = '#000';
 
     for (let link of links) {
       link.classList.remove('link-light');
       link.classList.add('link-dark');
     }
-    // links.forEach(function (link) {
-    //   link.classList.remove('link-light');
-    // });
   } else {
     myNav.classList.add('bg-transparent');
     myNav.classList.remove('bg-white');
+    icon.style.color = '#000';
 
     for (let link of links) {
       link.classList.remove('link-dark');
       link.classList.add('link-light');
     }
-    // links.forEach(function (link) {
-    //   link.classList.add('link-light');
-    // });
   }
 };
