eb1e6f6e1d8a96f88ea7bb083d4b9093de9fc037
karim <karimsouari101@gamil.com>
2022-09-10 15:30:11 +0000

    editing the form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const myNav = document.getElementById('mynav');
 const links = document.getElementsByClassName('nav-link');
+const icon = document.getElementById('icon')[0];
 console.log(links);
 window.onscroll = function () {
   console.log(window.scrollY);
@@ -7,6 +8,7 @@ window.onscroll = function () {
   if (window.scrollY >= 120) {
     myNav.classList.add('bg-white');
     myNav.classList.remove('bg-transparent');
+
     for (let link of links) {
       link.classList.remove('link-light');
       link.classList.add('link-dark');
@@ -17,6 +19,7 @@ window.onscroll = function () {
   } else {
     myNav.classList.add('bg-transparent');
     myNav.classList.remove('bg-white');
+
     for (let link of links) {
       link.classList.remove('link-dark');
       link.classList.add('link-light');
