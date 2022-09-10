f40e8556ef4dfc32d7bbd8f020edcd5cf0de593c
karim <karimsouari101@gamil.com>
2022-09-10 14:10:38 +0000

    fixed the navbar

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const myNav = document.getElementById('mynav');
+const links = document.getElementsByClassName('nav-link');
+console.log(links);
+window.onscroll = function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY >= 120) {
+    myNav.classList.add('bg-white');
+    myNav.classList.remove('bg-transparent');
+    for (let link of links) {
+      link.classList.remove('link-light');
+      link.classList.add('link-dark');
+    }
+    // links.forEach(function (link) {
+    //   link.classList.remove('link-light');
+    // });
+  } else {
+    myNav.classList.add('bg-transparent');
+    myNav.classList.remove('bg-white');
+    for (let link of links) {
+      link.classList.remove('link-dark');
+      link.classList.add('link-light');
+    }
+    // links.forEach(function (link) {
+    //   link.classList.add('link-light');
+    // });
+  }
+};
