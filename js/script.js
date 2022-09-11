c2fddce0fac164b9dda6092d7afe8276cce9a38b
karim <karimsouari101@gamil.com>
2022-09-11 13:16:34 +0000

    reset the nav with the toggler

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 const myNav = document.getElementById('mynav');
 const links = document.getElementsByClassName('nav-link');
-const icon = document.getElementById('icon');
-window.onscroll = function () {
-  console.log(icon);
+const navbarToggler = document.getElementById('navbar-toggler');
 
-  if (window.scrollY >= 120) {
-    myNav.classList.add('bg-white');
-    myNav.classList.remove('bg-transparent');
-    icon.style.color = '#000';
+// navbarToggler.addEventListener('click', () => {
+//   navbarToggler.classList.add('nav-toggler');
+// });
 
-    for (let link of links) {
-      link.classList.remove('link-light');
-      link.classList.add('link-dark');
-    }
-  } else {
-    myNav.classList.add('bg-transparent');
-    myNav.classList.remove('bg-white');
-    icon.style.color = '#000';
+// window.onscroll = function () {
+//   console.log(icon);
 
-    for (let link of links) {
-      link.classList.remove('link-dark');
-      link.classList.add('link-light');
-    }
-  }
-};
+//   if (window.scrollY >= 120) {
+//     myNav.classList.add('bg-white');
+//     myNav.classList.remove('bg-transparent');
+//     icon.style.color = '#000';
+
+//     for (let link of links) {
+//       link.classList.remove('link-light');
+//       link.classList.add('link-dark');
+//     }
+//   } else {
+//     myNav.classList.add('bg-transparent');
+//     myNav.classList.remove('bg-white');
+//     icon.style.color = '#fff';
+
+//     for (let link of links) {
+//       link.classList.remove('link-dark');
+//       link.classList.add('link-light');
+//     }
+//   }
+// };
